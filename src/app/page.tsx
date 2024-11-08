076cd43517a3ba481aad59d3100c3cf7c8fdeaf5
akkie8 <akkie.819@gmail.com>
2024-11-09 02:11:35 +0900

    feat: ヘッダーとフッターを追加し、ナビゲーションを強化

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,7 @@
-import Image from 'next/image';
-import { Button } from '@/components/ui/button';
-
 export default function Home() {
   return (
     <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
-      <main className='flex flex-col gap-8 row-start-2 items-center sm:items-start'>
-        <Image
-          className='dark:invert'
-          src='/next.svg'
-          alt='Next.js logo'
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className='list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]'>
-          <li className='mb-2'>
-            Get started by editing{' '}
-            <code className='bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold'>
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className='flex gap-4 items-center flex-col sm:flex-row'>
-          <Button>Deploy now</Button>
-          <Button>Read our docs</Button>
-        </div>
-      </main>
-      <footer className='row-start-3 flex gap-6 flex-wrap items-center justify-center'>
-        <p>
-          Made with ❤️ by{' '}
-          <a href='https://twitter.com/leerob' target='_blank' rel='noopener noreferrer'>
-            Lee Robinson
-          </a>
-        </p>
-      </footer>
+      <main className='flex flex-col gap-8 row-start-2 items-center sm:items-start'></main>
     </div>
   );
 }
