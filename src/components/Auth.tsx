922443fa3749ed250b310e88e52872b4a0daf97b
akkie8 <akkie.819@gmail.com>
2024-11-16 12:34:53 +0900

    fix: 環境変数名変更

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -7,7 +7,7 @@ import { useEffect, useState } from 'react';
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON!
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
 
 export default function AuthUI() {
