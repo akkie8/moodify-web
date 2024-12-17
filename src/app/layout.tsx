0a977c6556d8b43e440f7323b56ff32fb19a737c
Risa Goto <omusubi5656dup@gmail.com>
2024-12-17 14:07:25 +0900

    fix:余計なdivタグ削除#1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,6 @@ export default function RootLayout({
           <main className='flex-grow flex'>{children}</main>
 
           <footer className='p-8 mt-4 text-center text-gray-500  bg-gray-50 '>
-            <div></div>
             <div className='text-sm flex gap-3 items-center justify-center'>
               <Link href='/about'>運営者情報</Link>
               <Link href='/notice'>特定商取引法に基づく表記</Link>
