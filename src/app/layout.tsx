fe8834a394cd4a545d77e4ea9a331ba60a1b1056
Risa Goto <omusubi5656dup@gmail.com>
2024-12-18 12:08:51 +0900

    fix:mt削除 p

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
 
           <main className='flex-grow flex'>{children}</main>
 
-          <footer className='p-8 pt-5 text-center text-gray-500 bg-gray-50'>
+          <footer className='p-4 text-center text-gray-500 bg-gray-50'>
             <div className='text-sm flex gap-3 items-center justify-center'>
               <Link href='/about'>運営者情報</Link>
               <Link href='/notice'>特定商取引法に基づく表記</Link>
