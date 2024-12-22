4b8c535665bac990efbda17848fead081b51fbee
Risa Goto <omusubi5656dup@gmail.com>
2024-12-22 15:18:07 +0900

    fix:レスポンシブmd追加#10

--- src/app/(static)/about/page.tsx
+++ src/app/(static)/about/page.tsx
@@ -3,29 +3,29 @@ import Link from 'next/link';
 export default function CompanyInfo() {
   return (
     <div className='container mx-auto flex flex-col justify-center text-center px-4 py-16'>
-      <h1 className='text-2xl font-bold mb-8'>運営者情報</h1>
-      <h2 className='text-lg font-semibold mb-2'>サービス名</h2>
-      <p className='mb-4 text-sm'>Moodify</p>
+      <h1 className='text-2xl font-bold mb-8 md:text-3xl'>運営者情報</h1>
+      <h2 className='text-lg font-semibold mb-2 md:text-2xl'>サービス名</h2>
+      <p className='mb-4 text-sm md:text-base'>Moodify</p>
 
-      <h2 className='text-lg font-semibold mb-2'>運営者</h2>
-      <p className='mb-4 text-sm'>山田 亜樹（個人事業主）</p>
+      <h2 className='text-lg font-semibold mb-2 md:text-2xl'>運営者</h2>
+      <p className='mb-4 text-sm md:text-base'>山田 亜樹（個人事業主）</p>
 
-      <h2 className='text-lg font-semibold mb-2'>事業内容</h2>
-      <p className='mb-4 text-sm'>
+      <h2 className='text-lg font-semibold mb-2 md:text-2xl'>事業内容</h2>
+      <p className='mb-4 text-sm md:text-base'>
         - ウェブサービス「Moodify」の企画・開発・運営
         <br />
         - 感情分析AIの研究開発
         <br />- メンタルヘルスに関するコンサルティング
       </p>
 
-      <h2 className='text-lg font-semibold mb-2'>連絡先</h2>
-      <p className='mb-4 text-sm'>
+      <h2 className='text-lg font-semibold mb-2 md:text-2xl'>連絡先</h2>
+      <p className='mb-4 text-sm md:text-base'>
         メールアドレス: info@moodify.com
         <br />
         ※お問い合わせはメールでのみ受け付けております。
       </p>
 
-      <p className='mt-8 text-sm'>
+      <p className='mt-8 text-sm md:text-base'>
         <Link href='/' className='text-blue-600 hover:underline'>
           ホームに戻る
         </Link>
