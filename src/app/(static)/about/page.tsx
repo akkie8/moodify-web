538d01aefa11fbbebd8903a53c6cf384cc838023
Risa Goto <omusubi5656dup@gmail.com>
2024-12-22 23:44:37 +0900

    fix:md文字サイズ#10

--- src/app/(static)/about/page.tsx
+++ src/app/(static)/about/page.tsx
@@ -4,13 +4,13 @@ export default function CompanyInfo() {
   return (
     <div className='container mx-auto flex flex-col justify-center px-4 py-16 text-center'>
       <h1 className='font-bold text-2xl mb-8 md:mb-10 md:text-4xl'>運営者情報</h1>
-      <h2 className='mb-2 text-lg font-semibold  md:mb-4 md:text-3xl'>サービス名</h2>
+      <h2 className='mb-2 text-lg font-semibold  md:mb-4 md:text-2xl'>サービス名</h2>
       <p className='mb-4 text-sm md:text-xl'>Moodify</p>
 
-      <h2 className='mb-2 text-lg font-semibold md:mb-4 md:text-3xl'>運営者</h2>
+      <h2 className='mb-2 text-lg font-semibold md:mb-4 md:text-2xl'>運営者</h2>
       <p className='mb-4 text-sm md:text-xl'>山田 亜樹（個人事業主）</p>
 
-      <h2 className='mb-2 text-lg font-semibold md:mb-4 md:text-3xl'>事業内容</h2>
+      <h2 className='mb-2 text-lg font-semibold md:mb-4 md:text-2xl'>事業内容</h2>
       <p className='mb-4 text-sm md:text-xl'>
         - ウェブサービス「Moodify」の企画・開発・運営
         <br />
@@ -18,7 +18,7 @@ export default function CompanyInfo() {
         <br />- メンタルヘルスに関するコンサルティング
       </p>
 
-      <h2 className='mb-2 text-lg font-semibold md:mb-4 md:text-3xl'>連絡先</h2>
+      <h2 className='mb-2 text-lg font-semibold md:mb-4 md:text-2xl'>連絡先</h2>
       <p className='mb-4 text-sm md:text-xl'>
         メールアドレス: info@moodify.com
         <br />
