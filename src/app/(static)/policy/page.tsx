f2133fb55a72f119e6ed05499669e7f3110786f9
Risa Goto <omusubi5656dup@gmail.com>
2024-12-29 15:16:12 +0900

    fix:セクション事に背景カラー追加#20

--- src/app/(static)/policy/page.tsx
+++ src/app/(static)/policy/page.tsx
@@ -7,24 +7,26 @@ export default function PrivacyPolicy() {
       <p className='mb-4'>
         Moodify（以下、「当サービス」）は、ユーザーの個人情報保護を重要視し、以下のプライバシーポリシーを定めています。
       </p>
-      <h2 className='mb-2 text-sm'>1. 収集する情報</h2>
+      <h2 className='p-2 mb-4 tracking-widest text-base bg-gray-100'>1. 収集する情報</h2>
       <p className='mb-5 pb-5 border-b'>
         当サービスは、サービス提供に必要な範囲で以下の情報を収集します： -
         ユーザー登録情報（メールアドレス、パスワード等） - 利用履歴 - 感情記録データ
       </p>
-      <h2 className='mb-2 text-sm'>2. 情報の利用目的</h2>
+      <h2 className='p-2 mb-4 tracking-widest text-base bg-gray-100'>2. 情報の利用目的</h2>
       <p className='mb-5 pb-5 border-b'>
         収集した情報は、サービスの提供・改善、ユーザーサポート、統計データの作成に利用します。
       </p>
-      <h2 className='mb-2 text-sm'>3. 情報の管理</h2>
+      <h2 className='p-2 mb-4 tracking-widest text-base bg-gray-100'>3. 情報の管理</h2>
       <p className='mb-5 pb-5 border-b'>
         当サービスは、ユーザーの個人情報を適切に管理し、不正アクセス、紛失、破壊、改ざん、漏洩などを防ぐため、セキュリティ対策を実施します。
       </p>
-      <h2 className='mb-2 text-sm'>4. 第三者への提供</h2>
+      <h2 className='p-2 mb-4 tracking-widest text-base bg-gray-100'>4. 第三者への提供</h2>
       <p className='mb-5 pb-5 border-b'>
         法令に基づく場合を除き、ユーザーの同意なく第三者に個人情報を提供することはありません。
       </p>
-      <h2 className='mb-2 text-sm'>5. プライバシーポリシーの変更</h2>
+      <h2 className='p-2 mb-4 tracking-widest text-base bg-gray-100'>
+        5. プライバシーポリシーの変更
+      </h2>
       <p className='mb-5 pb-5 border-b'>
         当サービスは、必要に応じて本ポリシーを変更することがあります。変更後のポリシーは、本ページで公開された時点で効力を生じるものとします。
       </p>
